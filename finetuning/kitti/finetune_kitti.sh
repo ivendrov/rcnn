ce34447049037b7340e6e02918a34a4bf8c5e261
Ivan Vendrov <ivendrov@gmail.com>
2014-12-26 16:02:11 -0500

    added gpu_lock command to finetuning script

--- finetuning/kitti/finetune_kitti.sh
+++ finetuning/kitti/finetune_kitti.sh
@@ -1,1 +1,4 @@
-GLOG_logtostderr=1 /pkgs/caffe-master/build/tools/caffe.bin train -solver "kitti_finetune_solver.prototxt" -weights "../../data/caffe_nets/ilsvrc_2012_train_iter_310k" 2>&1 | tee log.txt
+ID=`/u/murray/bin/gpu_lock.py --id`
+GLOG_logtostderr=1 /pkgs/caffe-master/build/tools/caffe.bin train -solver "kitti_finetune_solver.prototxt" -weights "../../data/caffe_nets/ilsvrc_2012_train_iter_310k" -gpu $ID 2>&1 | tee log.txt 
+
+
